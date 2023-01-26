f337fd6ff92f7c7b136b7391562fb5efcc4f490f
Mauro Scheltens <mauroscheltens@hotmail.com>
2023-01-26 20:53:18 +0100

    feat: made component of navigation

---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,17 @@
+<nav>
+    <ul class='text-white flex justify-between items-center'>
+        <div class='flex justify-center'>
+            <img src='{{ asset('images/teypo.png') }}'>
+            <h2 class='ml-4 text-4xl'>teypo.</h2>
+        </div>
+        <div class='flex'>
+            <li class='cursor-pointer text-[#CAFC01]'>Home</li>
+            <li class='ml-16 cursor-pointer hover:text-[#CAFC01]'>Services</li>
+            <li class='ml-16 cursor-pointer hover:text-[#CAFC01]'>Blog</li>
+            <li class='ml-16 cursor-pointer hover:text-[#CAFC01]'>Contact</li>
+        </div>
+        <div>
+            <button class='bg-[#CAFC01] rounded-full text-black py-2 px-4 font-bold'>Contact us</button>
+        </div>
+    </ul>
+</nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,23 +7,7 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body class='bg-[#0E0E0E] p-8'>
-        <nav>
-            <ul class='text-white flex justify-between items-center'>
-                <div class='flex justify-center'>
-                    <img src='{{ asset('images/teypo.png') }}'>
-                    <h2 class='ml-4 text-4xl'>teypo.</h2>
-                </div>
-                <div class='flex'>
-                    <li class='cursor-pointer text-[#CAFC01]'>Home</li>
-                    <li class='ml-16 cursor-pointer hover:text-[#CAFC01]'>Services</li>
-                    <li class='ml-16 cursor-pointer hover:text-[#CAFC01]'>Blog</li>
-                    <li class='ml-16 cursor-pointer hover:text-[#CAFC01]'>Contact</li>
-                </div>
-                <div>
-                    <button class='bg-[#CAFC01] rounded-full text-black py-2 px-4 font-bold'>Contact us</button>
-                </div>
-            </ul>
-        </nav>
+        @include('components.navigation')
         <section>
             {{-- TODO: big text here (check figma) --}}
         </section>
