824accb32ec2d8c4784968075e33d71fbdd19f2e
Mauro Scheltens <mauroscheltens@hotmail.com>
2023-01-26 20:47:01 +0100

    feat: added navigation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,26 @@
         <title>Teypo | Begin your adventure</title>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
-    <body class='bg-[#0E0E0E]'>
-        <h1 class="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+    <body class='bg-[#0E0E0E] p-8'>
+        <nav>
+            <ul class='text-white flex justify-between items-center'>
+                <div class='flex justify-center'>
+                    <img src='{{ asset('images/teypo.png') }}'>
+                    <h2 class='ml-4 text-4xl'>teypo.</h2>
+                </div>
+                <div class='flex'>
+                    <li class='cursor-pointer text-[#CAFC01]'>Home</li>
+                    <li class='ml-16 cursor-pointer hover:text-[#CAFC01]'>Services</li>
+                    <li class='ml-16 cursor-pointer hover:text-[#CAFC01]'>Blog</li>
+                    <li class='ml-16 cursor-pointer hover:text-[#CAFC01]'>Contact</li>
+                </div>
+                <div>
+                    <button class='bg-[#CAFC01] rounded-full text-black py-2 px-4 font-bold'>Contact us</button>
+                </div>
+            </ul>
+        </nav>
+        <section>
+            {{-- TODO: big text here (check figma) --}}
+        </section>
     </body>
 </html>
